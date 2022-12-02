2aa189e9a77fb141438761a7e2edab897f855cdf
sir-toby <sir-toby@users.noreply.github.com>
2022-12-02 12:27:19 +0100

    day2 part 2

--- Code/02.py
+++ Code/02.py
@@ -1,6 +1,5 @@
 from utils import imports
 
-
 def transformToNumbers(game):
     game2 = [0,0]
     match game[0]:
@@ -17,6 +16,7 @@ def transformToNumbers(game):
 
 def result(game):
     game = transformToNumbers(game)
+    #const who wins:
     #1:1 draw
     #1:2 right
     #1:3 left
@@ -26,15 +26,25 @@ def result(game):
     #3:1 right
     #3:2 left
     #3:3 draw
-    difference = (game[1]-game[0])%3
-    if difference == 1: return 6+game[1]
-    if difference == 0: return 3+game[1]
-    if difference == 2: return game[1]
+    match (game[1]-game[0])%3:
+        case 1: return 6+game[1] #win
+        case 0: return 3+game[1]
+        case 2: return game[1]
+
+def result2(game):
+    game = transformToNumbers(game)
+    match game[1]:
+        #modulo +1 at the end to keep "3" in there
+        case 1: return (game[0]+1)%3+1 #lose --> 2 items down the list
+        case 2: return 3+game[0] #draw
+        case 3: return 6+(game[0])%3+1 #win --> 1 item down the list
 
 #Main:
 rpslist = imports.import2d('02 Input.txt', '\n', ' ')
-#Part1:
-points=0
+
+points1=0
+points2=0
 for game in rpslist: 
-    points += result(game)
-print(points)
+    points1 += result(game)
+    points2  += result2(game)
+print('Part 1: ' + str(points1) + ', Part 2: ' + str(points2))
