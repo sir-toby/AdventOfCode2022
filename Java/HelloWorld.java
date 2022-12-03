6929f232cd3cf1502c39af5ab3f1e2b97e7cef99
sir-toby <sir-toby@users.noreply.github.com>
2022-12-03 20:01:43 +0100

    hello-world shenenigans

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -2,6 +2,19 @@ package Java;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    // This is a single line output print
+    String helloWorld = "Hello World";
+    int x = 3, y = 4;
+
+    System.out.println(helloWorld);
+    System.out.println(Math.max(x, y));
+
+    /*
+     * The next two lines
+     * are producing text that is output
+     * to the same line
+     */
+    // System.out.print("same line");
+    // System.out.print("Example");
   }
 }
