005c9524b191f6c512a56baae607a215cd0a8ed5
sir-toby <sir-toby@users.noreply.github.com>
2022-12-10 12:03:21 +0100

    small tidying for day 8 part 1

--- Python/08.py
+++ Python/08.py
@@ -3,30 +3,37 @@ from utils import imports
 
 def isVisible(y, x, treeMap):
     transposedTreeMap = list(zip(*treeMap))
-    if (checkVisibility(transposedTreeMap[x][:y], treeMap[y][x]) or  # Up
-        checkVisibility(transposedTreeMap[x][y+1:], treeMap[y][x]) or  # Down
-        checkVisibility(treeMap[y][:x], treeMap[y][x]) or  # Left
-            checkVisibility(treeMap[y][x+1:], treeMap[y][x])):  # right
+    if (checkVisibilityInDirection(transposedTreeMap[x][:y], treeMap[y][x]) or  # Up
+        checkVisibilityInDirection(transposedTreeMap[x][y+1:], treeMap[y][x]) or  # Down
+        checkVisibilityInDirection(treeMap[y][:x], treeMap[y][x]) or  # Left
+            checkVisibilityInDirection(treeMap[y][x+1:], treeMap[y][x])):  # right
         return True
     else:
         return False
 
 
-def checkVisibility(treeList, tree):
+def checkVisibilityInDirection(treeList, tree):
     for compTree in treeList:
         if compTree >= tree:
             return False
     return True
 
 
+def countVisibleTrees(treeMap):
+    visibleCount = 2*(len(treeMap) + len(treeMap[0]))-4
+
+    for y in range(1, len(treeMap)-1, 1):
+        for x in range(1, len(treeMap[0])-1, 1):
+            if isVisible(y, x, treeMap) == True:
+                visibleCount += 1
+    return visibleCount
+
+
 # Main
 treeMap = imports.genericImport("08 Input.txt", ["\n", ""])
 
-visibleCount = 2*(len(treeMap) + len(treeMap[0]))-4
+# Part1
+countVisibleTrees(treeMap)
 
-for y in range(1, len(treeMap)-1, 1):
-    for x in range(1, len(treeMap[0])-1, 1):
-        if isVisible(y, x, treeMap) == True:
-            visibleCount += 1
 
-print("Visible trees: " + str(visibleCount))
+print("Visible trees: " + str(countVisibleTrees(treeMap)))
