6d3ff3f2876a3ee93c60503e0c3f91b0a3ae1c44
sir-toby <sir-toby@users.noreply.github.com>
2022-12-10 15:59:18 +0100

    day10 pt 2 (ugly code)

--- Python/10.py
+++ Python/10.py
@@ -27,9 +27,49 @@ def checkIfInterestingCycle(cycle, x, cycleList, valueListForCycles):
     return cycleList, valueListForCycles
 
 
+def buildDisplay(instructions, lineLength, totalLength):
+    spritePos = 0
+    cycle = 0
+    display = []
+    for instruction in instructions:
+        match instruction[0]:
+            case "noop":
+                display.append(drawChar(spritePos, cycle, lineLength))
+                cycle += 1
+            case "addx":
+                display.append(drawChar(spritePos, cycle, lineLength))
+                cycle += 1
+                display.append(drawChar(spritePos, cycle, lineLength))
+                cycle += 1
+                spritePos += int(instruction[1])
+
+    finalDisplay = []
+    line = ""
+    for i in range(totalLength):
+        line += display[i]
+        if len(line) == lineLength:
+            finalDisplay.append(line)
+            line = ""
+
+    return finalDisplay
+
+
+def drawChar(spritePos, cycle, lineLength):
+    if cycle % lineLength >= spritePos and cycle % lineLength <= spritePos+2:
+        return "#"
+    else:
+        return "."
+
+
 # Main
 instructions = imports.genericImport("10 Input.txt", ["\n", " "])
-interestingCycles = [20, 60, 100, 140, 180, 220]
 
 # Part1:
+interestingCycles = [20, 60, 100, 140, 180, 220]
 print(sum(calculateValueFor(interestingCycles, instructions)))
+
+# Part2:
+lineLength = 40
+totalLength = 240
+lines = buildDisplay(instructions, lineLength, totalLength)
+print("\n".join(lines))
